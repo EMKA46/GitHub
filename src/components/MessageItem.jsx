d4bf531a8917aa5cae40c6d37f145343b71b310e
EMKA46 <emkazamirov@gmail.com>
2025-05-25 14:09:15 +0600

    Update MessageItem.jsx

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 
-const MessageItem = ({ sms, time }) => {
+const MessageItem = ({ sms, time, onDelete, onCheck, isRead }) => {
   return (
-    <div style={styles.item}>
-      {sms} ({time})
-    </div>
+    <ItemContainer isRead={isRead}>
+      {sms} ({time}) {isRead && <span>прочитано</span>}
+      <DeleteButton onClick={onDelete}>
+        <TrashIcon src="https://w7.pngwing.com/pngs/111/249/png-transparent-computer-icons-rubbish-bins-waste-paper-baskets-symbol-miscellaneous-cdr-text.png" />
+      </DeleteButton>
+      <input type="checkbox" checked={isRead} onChange={onCheck} />
+    </ItemContainer>
   );
 };
 
-const styles = {
-  item: {
-    marginBottom: "5px",
-    color: "red",
-  },
-};
+// Стили
+
+const ItemContainer = styled.div`
+  margin-bottom: 5px;
+  color: black;
+  background-color: ${(props) => (props.isRead ? "#0acc0a" : "red")};
+  padding: 5px;
+  border-radius: 5px;
+`;
+
+const DeleteButton = styled.button`
+  background-color: white;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const TrashIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  background-color: darkred;
+`;
+
 export default MessageItem;
