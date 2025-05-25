aed3c1d6122a2c5343450b47ffdc7d737f9e8720
EMKA46 <emkazamirov@gmail.com>
2025-05-25 14:03:19 +0600

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 import ChatHeader from "./ChatHeader";
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
+const ChatContainer = styled.div`
+  width: 300px;
+  height: 400px;
+  border: 2px solid red;
+  border-radius: 10px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #fff;
+`;
+
+const Chat = ({ messages, setMessages }) => {
   const addMessage = (text) => {
     const newMessage = {
       id: Date.now(),
@@ -17,28 +29,33 @@ const Chat = () => {
     setMessages([...messages, newMessage]);
   };
 
+  const deleteMessage = (id) => {
+    setMessages(messages.filter((msg) => msg.id !== id));
+  };
+
+  const backgroundMessage = (id) => {
+    setMessages(
+      messages.map((msg) =>
+        msg.id === id
+          ? { ...msg, background: msg.background ? null : "#0acc0a" }
+          : msg
+      )
+    );
+  };
+
   return (
-    <div style={styles.chat}>
+    <ChatContainer>
       <ChatHeader name="Nurs Agai" />
       <p className="agai">Э ищяк где домашка (10:30)</p>
-      <MessageList messages={messages} />
+      <MessageList
+        messages={messages}
+        deleteMessage={deleteMessage}
+        backgroundMessage={backgroundMessage}
+      />
       <MessageInput onSend={addMessage} />
-    </div>
+    </ChatContainer>
   );
 };
 
-const styles = {
-  chat: {
-    width: "300px",
-    height: "400px",
-    border: "2px solid red",
-    borderRadius: "10px",
-    padding: "10px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    backgroundColor: "#fff",
-  },
-};
-
 export default Chat;
+``
