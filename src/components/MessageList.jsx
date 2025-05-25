8ac4aa93c4c45466b770b51faf343b0b8309b37a
EMKA46 <emkazamirov@gmail.com>
2025-05-25 14:09:30 +0600

    Update MessageList.jsx

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,22 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import MessageItem from "./MessageItem";
 
-const MessageList = ({ messages }) => {
+const MessageList = ({ messages, deleteMessage, backgroundMessage }) => {
   return (
-    <div style={styles.list}>
+    <ListContainer>
       {messages.map((msg) => (
-        <MessageItem key={msg.id} sms={msg.sms} time={msg.time} /> 
+        <MessageItem
+          key={msg.id}
+          sms={msg.sms}
+          time={msg.time}
+          onDelete={() => deleteMessage(msg.id)}
+          onCheck={() => backgroundMessage(msg.id)}
+          isRead={!!msg.background}
+          background={msg.background}
+        />
       ))}
-    </div>
+    </ListContainer>
   );
 };
 
-const styles = {
-  list: {
-    flexGrow: 1,
-    overflowY: "auto",
-    padding: "5px",
-  },
-};
+const ListContainer = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+  padding: 5px;
+`;
 
 export default MessageList;
